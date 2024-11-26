38e1f81711ffff256f204c14f7517d0c6626a551
Mohammad-Haidar <163796786+Mohammad-Haidar@users.noreply.github.com>
2024-11-26 22:20:41 +0530

    1st Commit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slides');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Auto-play slides every 3 seconds
